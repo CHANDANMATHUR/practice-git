e42e2269015f4eb491e1959ff852627c290d04cd
cm <chandanmathur102018@gmail.com>
2024-02-29 14:25:36 +0530

    changes saved

--- Emp.java
+++ Emp.java
@@ -5,3 +5,21 @@ public interface Employee
 	void behavior();
 	void pressure();
 }
+
+public class Employee_Implementation_Class  implements Employee
+{
+	@Override
+	public void behavior() 
+	{
+		System.out.println("Employee Nature is Awesome");
+	}
+
+	@Override
+	public void pressure() 
+	{
+		System.out.println("Companies are giving more pressure to Employees");
+	}
+	
+
+}
+
